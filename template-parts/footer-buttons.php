53d7f435bff1ecfd08cbd6edc662e4d1b6343b5a
Megumin <Chiaki@users.noreply.github.com>
2020-08-18 17:20:43 +0300

    New link fot button

--- template-parts/footer-buttons.php
+++ template-parts/footer-buttons.php
@@ -11,5 +11,5 @@
 
 			<div class="text-center pt20 mb40">
                 <a href="https://forms.gle/k4sKc581Anh5STxM9" target="_blank" class="btn btn-primary btn-lg btn-rounded mr40 ml40 mt20 mb20">Добавить инициативу</a>
-                <a href="https://docs.google.com/forms/d/1PWQ-08rTet1930pwdlRu2cSKA_VsYVV1kzM5diXS8gQ/viewform?edit_requested=true" target="_blank" class="btn btn-primary btn-lg btn-rounded mr40 ml40 mt20 mb20">Стать волонтером</a>
+                <a href="https://help.sysman.su/pub/form/7_prisoedinitsya/h2xd2v/" target="_blank" class="btn btn-primary btn-lg btn-rounded mr40 ml40 mt20 mb20">Стать волонтером</a>
             </div>
